2edb50bea29aa5201ce9085b368729acf04e7d47
Kevin Sallee <kevin.sallee@gpltech.com>
2025-01-27 10:25:34 -0600

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ssml-maker",
-    version="0.1.0",
+    version="0.1.1",
     author="Kevin Sallée",
     author_email="kevin.sallee@gmail.com",
     description="A Python library for building SSML (Speech Synthesis Markup Language) documents",
